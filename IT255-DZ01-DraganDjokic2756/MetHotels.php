44f9bfc9aa2c60a2060de936ab67f44f96ed97db
Dragan <originalbree@gmail.com>
2017-06-06 22:47:49 +0200

    Add files via upload

---
+++ IT255-DZ01-DraganDjokic2756/MetHotels.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+    		<div class="row">
+    			<h3>IT255-DZ01</h3>
+    		</div>
+			<div class="row">
+				<table class="table table-striped table-bordered">
+		              <thead>
+		                <tr>
+		                  <th>Dragan</th>
+		                  <th>Djokic</th>
+		                  <th>2756</th>
+		                  <th>IT</th>
+		                  <th>FIT</th>
+		                </tr>
+		              </thead>
+	            </table>
+    		</div>
+    	</div>
+  	</body>
+</html>
